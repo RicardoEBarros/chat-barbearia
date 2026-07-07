08e04ba68a40e33b630b90db4d463dd6d41545fd
Ricardo Barros <rcebarros@gmail.com>
2026-07-07 09:19:02 -0300

    feat(ok): definir valor padrão para o parâmetro data

--- src/common/presentation/helpers/http-helper.ts
+++ src/common/presentation/helpers/http-helper.ts
@@ -1,7 +1,7 @@
 import { StatusCode } from '@/common/presentation/enums/status-code'
 import { HttpResponse } from '@/common/presentation/protocols/http'
 
-export const ok = (data: any): HttpResponse => ({
+export const ok = (data: any = {}): HttpResponse => ({
     statusCode: StatusCode.ok,
     body: data
 })
