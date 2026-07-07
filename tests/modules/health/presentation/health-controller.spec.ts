e724a885eb94add3dd5f3676ed5c22a59d96a526
Ricardo Barros <rcebarros@gmail.com>
2026-07-07 09:22:49 -0300

    test(HealthController): utilizar o StatusCode.ok ao invés do valor 200

--- tests/modules/health/presentation/health-controller.spec.ts
+++ tests/modules/health/presentation/health-controller.spec.ts
@@ -1,13 +1,14 @@
 import { describe, it, expect } from '@jest/globals'
 
 import { HealthController } from '@/modules/health/presentation/health-controller'
+import { StatusCode } from '@/common/presentation/enums/status-code'
 
 describe('HealthController Test Suíte', () => {
 
     it('Deve retornar o status code 200 se tudo der certo', async () => {
         const sut = new HealthController()
         const resposta = await sut.handler()
-        expect(resposta.statusCode).toBe(200)
+        expect(resposta.statusCode).toBe(StatusCode.ok)
     })
 
 })
