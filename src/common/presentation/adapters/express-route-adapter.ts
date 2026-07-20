f10df739767fbf82fae15b51a8402ba252436efb
Ricardo Barros <rcebarros@gmail.com>
2026-07-20 13:38:15 -0300

    fix(expressRouteAdapter): simplificar forma de verificação da existência da propriedade body do request

--- src/common/presentation/adapters/express-route-adapter.ts
+++ src/common/presentation/adapters/express-route-adapter.ts
@@ -4,12 +4,10 @@ import { StatusCode } from '../enums/status-code'
 import { Controller } from '../protocols/controller.protocol'
 import { HttpRequest } from '../protocols/http'
 
-const METHODS_WITH_BODY_PARAMETER = [ 'post', 'put', 'patch' ]
-
 export const adaptRoute = (controller: Controller) => {
     return async (req: Request, res: Response) => {
         let httpRequest: HttpRequest | undefined
-        if (METHODS_WITH_BODY_PARAMETER.includes(req.method.toLowerCase())) {
+        if (req?.body) {
             httpRequest = { body: req.body }
         }
         await controller.handler(httpRequest)
