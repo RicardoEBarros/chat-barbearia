d00b81064ae67a3a34ddfa9c4278a7e0b6160682
Ricardo Barros <rcebarros@gmail.com>
2026-07-20 13:16:41 -0300

    feat(expressRouteAdapter): garantir que o método handler seja chamado com o parâmetro correto

---
+++ src/common/presentation/adapters/express-route-adapter.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+import { StatusCode } from '../enums/status-code'
+import { Controller } from '../protocols/controller.protocol'
+import { HttpRequest } from '../protocols/http'
+
+export const adaptRoute = (controller: Controller) => {
+    return async (req: Request, res: Response) => {
+        const httpRequest: HttpRequest = { body: req.body }
+        await controller.handler(httpRequest)
+        res.status(StatusCode.ok).json({})
+    }
+}
+
