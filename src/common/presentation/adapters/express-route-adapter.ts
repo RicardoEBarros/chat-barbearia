cd9142b062a48f273f652eeadeab9380ffcbe5d5
Ricardo Barros <rcebarros@gmail.com>
2026-07-20 13:32:01 -0300

    feat(expressRouteAdapter): garantir que handler não receba o parâmetro body se método for GET

--- src/common/presentation/adapters/express-route-adapter.ts
+++ src/common/presentation/adapters/express-route-adapter.ts
@@ -4,9 +4,14 @@ import { StatusCode } from '../enums/status-code'
 import { Controller } from '../protocols/controller.protocol'
 import { HttpRequest } from '../protocols/http'
 
+const METHODS_WITH_BODY_PARAMETER = [ 'post', 'put', 'patch' ]
+
 export const adaptRoute = (controller: Controller) => {
     return async (req: Request, res: Response) => {
-        const httpRequest: HttpRequest = { body: req.body }
+        let httpRequest: HttpRequest | undefined
+        if (METHODS_WITH_BODY_PARAMETER.includes(req.method.toLowerCase())) {
+            httpRequest = { body: req.body }
+        }
         await controller.handler(httpRequest)
         res.status(StatusCode.ok).json({})
     }
