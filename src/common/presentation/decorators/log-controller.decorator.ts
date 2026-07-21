53c3201492e95a11f5eaa9a7b707e6af9499d133
Ricardo Barros <rcebarros@gmail.com>
2026-07-21 11:13:14 -0300

    feat(LogControllerDecorator): garantir que não chame logError se status code for da família 200

--- src/common/presentation/decorators/log-controller.decorator.ts
+++ src/common/presentation/decorators/log-controller.decorator.ts
@@ -5,10 +5,13 @@ import { LogError } from '../protocols/log-error.protocol'
 
 export class LogControllerDecorator implements Controller {
 
-    constructor(private readonly controller: Controller, private readonly logError: LogError) {}
+    constructor(private readonly controller: Controller, private readonly logErrorRepository: LogError) {}
 
     async handler(httpRequest?: HttpRequest): Promise<HttpResponse> {
-        await this.controller.handler(httpRequest)
+        const httpResponse = await this.controller.handler(httpRequest)
+        if (httpResponse.statusCode >= 500 && httpResponse.statusCode <= 599) {
+            await this.logErrorRepository.logError(httpResponse.body.stack)
+        }
         return ok()
     }
 
