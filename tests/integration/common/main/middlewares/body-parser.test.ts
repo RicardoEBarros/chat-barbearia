d1539e42c09cf5b98a48f1a4fcdd166442bb1960
Ricardo Barros <rcebarros@gmail.com>
2026-07-29 13:29:38 -0300

    refactor(bodyParser): gerar endpoint via object mother

--- tests/integration/common/main/middlewares/body-parser.test.ts
+++ tests/integration/common/main/middlewares/body-parser.test.ts
@@ -1,21 +1,21 @@
 import request from 'supertest'
-import { faker } from '@faker-js/faker'
 import { describe, it } from '@jest/globals'
 import { Request, Response } from 'express'
 
 import { app } from '@/common/main/config'
+import { UrlRelatedObjectMother } from '@/tests/mocks/object-mothers'
 
 describe('Body Parser Middleware Suíte', () => {
 
     it('Deve transformar body em json', async () => {
 
-        const url = `/${faker.internet.domainWord()}`
-        app.post(url, (req: Request, res: Response) => {
+        const endPoint = UrlRelatedObjectMother.endPoint()
+        app.post(endPoint, (req: Request, res: Response) => {
             res.send(req.body)
         })
 
         await request(app)
-            .post(url)
+            .post(endPoint)
             .send({ nome: 'Ricardo' })
             .expect({ nome: 'Ricardo' })
 
