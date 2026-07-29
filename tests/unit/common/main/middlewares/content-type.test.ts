042c99eac9993f9df8dbf6139ec5c09d0aaba191
Ricardo Barros <rcebarros@gmail.com>
2026-07-29 13:06:24 -0300

    test(contentType): garantir que retorne o tipo xml quando forçado a isso

--- tests/unit/common/main/middlewares/content-type.test.ts
+++ tests/unit/common/main/middlewares/content-type.test.ts
@@ -1,15 +1,19 @@
 import request from 'supertest'
-import { describe, it } from '@jest/globals'
+import { describe, it, beforeEach } from '@jest/globals'
 
 import { app } from '@/common/main/config'
 import { UrlRelatedObjectMother } from '@/tests/mocks/object-mothers'
 
 describe('ContentType Middleware Suíte', () => {
 
+    let endPoint: string
+
+    beforeEach(() => {
+        endPoint = UrlRelatedObjectMother.endPoint()
+    })
+
     it('Deve retornar o content-type default como json', async () => {
        
-        const endPoint = UrlRelatedObjectMother.endPoint()
-
         app.post(endPoint, (req, res) => {
             res.send()
         })
@@ -17,6 +21,19 @@ describe('ContentType Middleware Suíte', () => {
         await request(app)
             .post(endPoint)
             .expect('content-type', /json/)
+
+    })
+
+    it('Deve retornar o tipo xml quando forçado', async () => {
+
+        app.post(endPoint, (req, res) => {
+            res.type('xml')
+            res.send()
+        })
+
+        await request(app)
+            .post(endPoint)
+            .expect('content-type', /xml/)
 
     })
         
